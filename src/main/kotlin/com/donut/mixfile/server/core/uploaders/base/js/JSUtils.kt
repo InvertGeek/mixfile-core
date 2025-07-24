5f47d7eb92c3834d1eb364b146e55cf26aeaad77
1 <386029724@qq.com>
2025-07-24 17:38:53 +0800

    secure js scope

--- src/main/kotlin/com/donut/mixfile/server/core/uploaders/base/js/JSUtils.kt
+++ src/main/kotlin/com/donut/mixfile/server/core/uploaders/base/js/JSUtils.kt
@@ -5,6 +5,7 @@ import io.ktor.client.*
 import org.mozilla.javascript.Context
 import org.mozilla.javascript.Scriptable
 
+
 fun getContext(): Context = Context.enter().apply {
     //禁用优化(动态创建class)，否则无法在安卓运行
     optimizationLevel = -1
@@ -15,6 +16,21 @@ fun runScript(code: String, client: HttpClient = defaultClient, variables: Scrip
     val context = getContext()
     context.use {
         val scope = getRhinoScope(it, client)
+        listOf(
+            "Packages",
+            "getClass",
+            "JavaAdapter",
+            "JavaImporter",
+            "java",
+            "javax",
+            "org",
+            "com",
+            "edu",
+            "net",
+            "android"
+        ).forEach { name ->
+            scope.delete(name)
+        }
         scope.variables()
         val result = it.evaluateString(scope, code, "mixfile_uploader", 1, null)
         return result.toString()
