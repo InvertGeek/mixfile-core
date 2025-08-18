5366cb4f6696d4c84d7179ac6de0550cff745217
1 <386029724@qq.com>
2025-08-18 12:32:39 +0800

    +request js function

--- src/main/kotlin/com/donut/mixfile/server/core/uploaders/base/js/Scope.kt
+++ src/main/kotlin/com/donut/mixfile/server/core/uploaders/base/js/Scope.kt
@@ -26,7 +26,7 @@ fun <T> Scriptable.putFunc(name: String, func: (args: Array<out Any>) -> T?) {
             func(it)
         } catch (e: Exception) {
             //不能抛出Exception Rhino会提示无法将Exception cast为error
-            throw Error("JS函数(${name})执行出错: ${e.message}")
+            throw Error("JS函数(${name})执行出错: ${e.message}", e)
         }
     })
 }
@@ -117,32 +117,34 @@ fun getRhinoScope(context: Context, client: HttpClient): Scriptable {
         println(it.joinToString(" "))
     }
 
+    fun parseFormData(obj: NativeObject) = formData {
+        obj.forEach { entry ->
+            val data = entry.value
+            val key = entry.key.toString()
+            if (data is NativeArray) {
+                val fileInfo = data.toList().map { it.toString() }
+                val imageData = fileInfo[0].decodeBase64Bytes()
+                val fileName = fileInfo[1]
+                val contentType = fileInfo[2]
+                add(key, imageData, Headers.build {
+                    append(HttpHeaders.ContentType, contentType)
+                    append(
+                        HttpHeaders.ContentDisposition,
+                        "filename=\"${fileName}\""
+                    )
+                })
+                return@forEach
+            }
+            //处理 org.mozilla.javascript.ConsString
+            add(key, data.toString())
+        }
+    }
+
     scope.putFunc("submitForm") { args ->
         val url = args.param(0, "")
         val formValues = args.param(1, NativeObject())
         val reqHeaders = args.param(2, NativeObject())
-        val reqForm = formData {
-            formValues.forEach { entry ->
-                val data = entry.value
-                val key = entry.key.toString()
-                if (data is NativeArray) {
-                    val fileInfo = data.toList().map { it.toString() }
-                    val imageData = fileInfo[0].decodeBase64Bytes()
-                    val fileName = fileInfo[1]
-                    val contentType = fileInfo[2]
-                    add(key, imageData, Headers.build {
-                        append(HttpHeaders.ContentType, contentType)
-                        append(
-                            HttpHeaders.ContentDisposition,
-                            "filename=\"${fileName}\""
-                        )
-                    })
-                    return@forEach
-                }
-                //处理 org.mozilla.javascript.ConsString
-                add(key, data.toString())
-            }
-        }
+        val reqForm = parseFormData(formValues)
         runBlocking {
             client.submitFormWithBinaryData(
                 url,
@@ -169,6 +171,60 @@ fun getRhinoScope(context: Context, client: HttpClient): Scriptable {
                 }
                 setBody(reqBody)
             }.bodyAsBytes().encodeBase64()
+        }
+    }
+
+    scope.putFunc("request") { args ->
+
+        val reqMethod = args.param(0, "")
+
+        val reqUrl = args.param(1, "")
+
+        val reqHeaders = args.param(3, NativeObject())
+
+        val reqBody = args.param(2, Any()).let {
+            if (it is String) {
+                return@let it
+            }
+            if (it is NativeObject) {
+                return@let MultiPartFormDataContent(parseFormData(it))
+            }
+            if (it is NativeArray) {
+                val first = it.firstOrNull() ?: return@let null
+                return@let first.toString().decodeBase64Bytes()
+            }
+            return@let null
+        }
+
+
+        runBlocking {
+
+            val response = client.request {
+                method = HttpMethod(reqMethod.uppercase())
+                url(reqUrl)
+                reqHeaders.forEach {
+                    header(it.key.toString(), it.value)
+                }
+                setBody(reqBody)
+            }
+
+            val body = response.bodyAsBytes()
+
+            val headers = response.headers.let {
+                context.newObject(scope).apply {
+                    it.names().forEach { name ->
+                        put(name, it[name].toString())
+                    }
+                }
+            }
+
+            context.newObject(scope).apply {
+                put("statusCode", response.status.value)
+                put("headers", headers)
+                put("rawData", body.encodeBase64())
+                put("text", body.decodeToString())
+            }
+
         }
     }
 
